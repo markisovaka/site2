051aee33552a6baad756b6bfcf315a772cb039bc
Кристина <112832176+markisovaka@users.noreply.github.com>
2025-03-30 18:28:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ let tg = window.Telegram.WebApp
 let btn = document.querySelector("#click")
 
 
-btn.onClick = () =>{
-    if (tg.MainButton.isVisible){
+btn1.onclick = () => {
+    if (tg.MainButton.isVisible) {
         tg.MainButton.hide()
-    }else{
-        tg.MainButton.setText("qwertyuio")
+    } else {
+        tg.MainButton.setText("Вы выбрали Москва-Нижний Новгород!")
+        item = "Москва-Нижний Новгород"
         tg.MainButton.show()
     }
 }
+
 
 tg.expand()
